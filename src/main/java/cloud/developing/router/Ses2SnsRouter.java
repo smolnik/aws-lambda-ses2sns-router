3007daca7e2dc7d0a18b3fa572a265d9075d65a1
AD004SM <AD004SM@UK-B010LC2.pbi.global.pvt>
2016-12-15 13:55:13 +0100

    something working :)

--- src/main/java/cloud/developing/router/Ses2SnsRouter.java
+++ src/main/java/cloud/developing/router/Ses2SnsRouter.java
@@ -1,7 +1,15 @@
 package cloud.developing.router;
 
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.List;
+import java.util.Map;
+
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
+import com.amazonaws.services.sns.AmazonSNS;
+import com.amazonaws.services.sns.AmazonSNSClient;
 
 /**
  * @author asmolnik
@@ -9,9 +17,37 @@ import com.amazonaws.services.lambda.runtime.LambdaLogger;
  */
 public class Ses2SnsRouter {
 
-	public void route(String event, Context context) {
+	private final AmazonSNS sns = new AmazonSNSClient();
+
+	public void route(Map<String, Object> eventMap, Context context) {
 		LambdaLogger log = context.getLogger();
-		log.log(event);
+		List<Map<String, Object>> records = getListAsValue("Records", eventMap);
+		log.log(eventMap.toString());
+		records.forEach(m -> {
+			String source = getValue("source", getMap(m, "ses", "mail"));
+			ZonedDateTime now = Instant.now().atZone(ZoneId.systemDefault());
+			String message = "Mail has just been received from: " + source + ", on " + now.toLocalDate() + " at " + now.toLocalTime();
+			String targetTopicArn = System.getenv("targetTopicArn");
+			sns.publish(targetTopicArn, message);
+			log.log("message \"" + message + "\" has been published to " + targetTopicArn);
+		});
+	}
+
+	@SuppressWarnings("unchecked")
+	private Map<String, Object> getMap(Map<String, Object> map, String... keys) {
+		for (String key : keys) {
+			map = Map.class.cast(map.get(key));
+		}
+		return map;
+	}
+
+	@SuppressWarnings("unchecked")
+	private List<Map<String, Object>> getListAsValue(String key, Map<String, Object> map) {
+		return List.class.cast(map.get(key));
+	}
+
+	private String getValue(String key, Map<String, Object> map) {
+		return String.class.cast(map.get(key));
 	}
 
 }
